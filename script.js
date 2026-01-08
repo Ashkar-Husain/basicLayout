d8d1b96987dc663df15bb20030858949834928aa
Ashkar-Husain <ashkarhusain72@gmail.com>
2026-01-09 01:42:07 +0530

    Fix dropdown issue

--- script.js
+++ script.js
@@ -6,30 +6,26 @@ function toggleSidebar() {
   const accordions = document.querySelector(".accordions");
   aside.classList.toggle("open");
   sidebar_img.classList.toggle("open");
-  accordions.style.display = "none";
 
   if (aside.classList.contains("open")) {
     hamburger.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+    accordions.style.display = "none";
   } else {
     hamburger.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+    accordions.style.display = "block";
   }
 }
 
 //* Open/Close Accordions
-function toggleAccordions() {
-  const icons = document.querySelectorAll("small");
-  icons.forEach((icon) => {
-    let accordion = icon.closest(".accordion");
-    // console.log(accordion, "ashkar");
-    // const para = accordion.querySelector("p");
-    // console.log(para)
-    accordion.classList.toggle("close");
-    if (accordion.classList.contains("close")) {
-      icon.innerHTML = '<i class="fa fa-plus"></i>';
-    } else {
-      icon.innerHTML = '<i class="fa fa-minus"></i>';
-    }
-  });
+function toggleAccordions(el) {
+  const accordion = el.closest(".accordion");
+  accordion.classList.toggle("close");
+
+  if (accordion.classList.contains("close")) {
+    el.innerHTML = '<i class="fa fa-plus"></i>';
+  } else {
+    el.innerHTML = '<i class="fa fa-minus"></i>';
+  }
 }
 
 //* Active/Inactive Navigation
@@ -40,9 +36,9 @@ function getActiveNavigation() {
     const li = e.target.closest("li");
     if (!li) return;
 
-    nav.querySelectorAll("li").forEach(item =>
-      item.classList.remove("active")
-    );
+    nav
+      .querySelectorAll("li")
+      .forEach((item) => item.classList.remove("active"));
 
     li.classList.add("active");
   });
@@ -50,7 +46,16 @@ function getActiveNavigation() {
 
 getActiveNavigation();
 
-
+function toggleDropdown() {
+  const profile = document.querySelector(".profile");
+  profile.classList.toggle("close");
+  if (profile.classList.contains("close")) {
+    document
+      .querySelector(".fa-chevron-up")
+      .classList.remove("fa-chevron-down");
+  } else {
+    document.querySelector(".fa-chevron-up").classList.add("fa-chevron-down");
+  }
+}
 
 //!-- Highlight Text
-
